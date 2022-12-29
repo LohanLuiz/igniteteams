5f73de818c491a23d167ec517b83bf7c2c591916
Lohan Luiz <lohanluiz12@gmail.com>
2022-12-29 11:39:41 -0400

    Progress

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Amogus']);
+
   return (
     <Container>
       <Header/>
+
       <Highlight 
         title="Turmas"
         subtitle="Jogue com a sua turma"
       />
+
+      <GroupCard title="SUS" />
+      
     </Container>
   )
 }
