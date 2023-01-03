9dc31735921f9f77a09ddd6106e3d45d7b87b98c
Lohan Luiz <lohanluiz12@gmail.com>
2023-01-03 09:01:03 -0400

    Progress

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,15 @@ import { FlatList } from 'react-native';
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState(['Amogus']);
+  const [groups, setGroups] = useState<string[]>([
+    'Amogus',
+  ]);
 
   return (
     <Container>
@@ -19,8 +23,24 @@ export function Groups() {
         subtitle="Jogue com a sua turma"
       />
 
-      <GroupCard title="SUS" />
-      
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Lista vazia"/>
+        )}
+      />
+
+      <Button 
+        title="Criar nova turma"
+      />
+
     </Container>
   )
 }
