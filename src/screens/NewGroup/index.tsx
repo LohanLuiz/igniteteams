23df72bb451a627fc11fe102ce735ad79f94ef25
Lohan Luiz <lohanluiz12@gmail.com>
2023-01-05 22:27:09 -0400

    Progress

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from './styles'
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function NewGroup() {
@@ -16,8 +17,13 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar membros."
         />
 
+        <Input 
+          placeholder="Nome da Turma"
+        />
+
         <Button 
           title="Criar"
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
